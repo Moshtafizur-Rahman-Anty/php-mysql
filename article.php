a1d5d38b63f5fa18fe381ca0bb80508e483bfc0f
Moshtafizur <moshtafziur.rahman.anty@gmail.com>
2021-12-11 22:33:58 +0600

    included database.php

--- article.php
+++ article.php
@@ -1,19 +1,6 @@
 <?php
 
-$db_host = "localhost";
-$db_name = "cms";
-$db_user = "cms_www";
-$db_pass = "iamravs100";
-
-$conn = mysqli_connect( $db_host, $db_user, $db_pass, $db_name );
-
-if(mysqli_connect_error()) {
-
-    echo mysqli_connect_error();
-    exit;
-
-}
-
+include 'database.php';
 
 if(isset($_GET['id']) && is_numeric($_GET['id']) ) {
 
