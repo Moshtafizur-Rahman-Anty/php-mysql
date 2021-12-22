bfa30b334dae2a98692153c3a76f954d5b81bead
Moshtafizur <moshtafziur.rahman.anty@gmail.com>
2021-12-22 16:26:21 +0600

    escape input

--- new-article.php
+++ new-article.php
@@ -1,11 +1,35 @@
-<?php 
+<?php
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+require 'includes/database.php';
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $sql = "INSERT INTO article (title, content, pblished_at)
+            VALUES ('" . mysqli_escape_string( $connn,  $_POST['title'] ) . "',
+                   '"  . mysqli_escape_string( $conn, $_POST['content'] ). "',
+                   '"  . mysqli_escape_string( $conn, $_POST['published_at'] ). "')";
+
 
     
 
-}
+    $results = mysqli_query($conn, $sql);
 
+    if ($results === false) {
+
+        echo mysqli_error($conn);
+
+    } else {
+      
+      var_dump($sql);  
+
+      $id = mysqli_insert_id($conn);
+      echo "Inserted record with ID: $id";
+
+
+    }
+
+}
 
 ?>
 
@@ -31,7 +55,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <div>
         <label for="published_at">Publishication date and time</label>
-        <input type="datetime-local" name="pubished_at" id="published_at">
+        <input type="datetime-local" name="published_at" id="published_at">
     </div>
 
     <button>Add</button>
